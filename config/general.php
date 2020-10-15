e5cb39aca8e4017e7bf0d297fffb3b8099073d63
cbad <jmayall@surething.com>
2020-10-15 15:28:08 -0700

    initial commit

---
+++ config/general.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\helpers\App;
+
+return [
+    // Global settings
+    '*' => [
+
+        // Craft config settings from .env variables
+		'aliases' => array(
+			'basePath' => $_SERVER['DOCUMENT_ROOT'],
+            'baseUrl'  => "//" . $_SERVER['HTTP_HOST'],
+            '@web' => App::env('SITE_URL'),
+            '@webroot' => App::env('WEB_ROOT_PATH'),
+            '@assetUrl' => '@web/assets',
+            '@assetPath' => '@webroot/assets',                    
+        ),
+        'allowUpdates' => (bool)App::env('ALLOW_UPDATES'),
+        'allowAdminChanges' => (bool)App::env('ALLOW_ADMIN_CHANGES'),
+        'backupOnUpdate' => (bool)App::env('BACKUP_ON_UPDATE'),
+        'devMode' => (bool)App::env('DEV_MODE'),
+        'enableTemplateCaching' => (bool)App::env('ENABLE_TEMPLATE_CACHING'),
+        'isSystemLive' => (bool)App::env('IS_SYSTEM_LIVE'),
+        'resourceBasePath' => App::env('WEB_ROOT_PATH').'/cpresources',
+        'runQueueAutomatically' => (bool)App::env('RUN_QUEUE_AUTOMATICALLY'),
+        'securityKey' => App::env('SECURITY_KEY'),
+        'appID' => App::env('APP_ID'),
+        'siteUrl' => App::env('SITE_URL'),
+        'testToEmailAddress' => (bool)App::env('TEST_TO_EMAIL', false),
+
+        // Craft config settings from constants
+		'useProjectConfigFile' => false,
+        'sendPoweredByHeader' => false,	// why would anybody do this.. talk about increasing security risks
+        'defaultWeekStartDay' => 1,
+        'cpTrigger' => 'admin',        
+        'cacheDuration' => false,
+        'defaultSearchTermOptions' => [
+            'subLeft' => true,
+            'subRight' => true,
+        ],
+        'defaultTokenDuration' => 'P2W',
+        'enableCsrfProtection' => true,
+        'errorTemplatePrefix' => 'errors/',
+        'generateTransformsBeforePageLoad' => true,
+        'maxCachedCloudImageSize' => 3000,
+        'maxUploadFileSize' => '100M',
+        'omitScriptNameInUrls' => true,
+        'useEmailAsUsername' => true,
+        'usePathInfo' => true,    
+        
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+        'devMode' => true,
+
+        // Prevent crawlers from indexing pages and following links
+        'disallowRobots' => true,
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Set this to `false` to prevent administrative changes from being made on staging
+        'allowAdminChanges' => true,
+
+        // Prevent crawlers from indexing pages and following links
+        'disallowRobots' => true,
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Set this to `false` to prevent administrative changes from being made on production
+        'allowAdminChanges' => true,
+    ],
+];
---
+++ web/assets/js/main.js
@@ -0,0 +1,106 @@
+function test() {
+	return;
+}
+
+
+// @ts-ignore
+$(document).ready(function () {
+
+	/*
+	 * detect hover enabled devices
+	 */
+	var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+	if (touchsupport){ // browser doesn't support touch
+		$("body").addClass("has-touch");
+		$(".MenuChoices .MenuChoices-overlay").css("display","block");
+	}	
+	else {
+	}
+
+	/*
+	 * initialize lazyloading
+	 */
+	var lazyLoad = new LazyLoad({
+		elements_selector: ".lazy",
+		cancel_on_exit: true
+	});
+
+	/*
+	 * add mobile menu management
+	 */
+	var gNavMenu = '#global-nav-menu';
+	$('#global-nav-slick').slicknav({
+		label: '',
+		prependTo: gNavMenu,
+		closeOnClick: true,
+		showChildren: true
+	});
+
+	/*
+	 * toggle availability off drop-down when in mobile menu mode
+	 */
+	var appMenuMode = "desktop";
+	$(window).resize(function () {
+		var width = $(window).width();
+		if ((width <= 750) && (appMenuMode === 'desktop')) {
+			appMenuMode = "mobile";
+			$('.nav__parent').removeClass('sub-menu-parent');
+			$('.nav__childmenu').removeClass('sub-menu');
+			$('.slicknav_nav .nav__link').removeClass('btn btn--buy btn--small');
+		}
+		else if ((width > 750) && (appMenuMode === 'mobile')) {
+			appMenuMode = "desktop";
+			$('.nav__parent').addClass('sub-menu-parent');
+			$('.nav__childmenu').addClass('sub-menu');
+		}
+	});
+	$(window).trigger("resize");
+
+	/*
+	 * add/remove local nav on scroll
+	 */
+	var win = $(window);
+
+	/* before changes 
+	win.scroll(function (event) {
+		if (win.scrollTop() > 130) {
+			$(".MainHdr").removeClass("nav--goin-out");
+			$(".MainHdr").addClass("MainHdr--down");
+			$(".HdrNavRow-Container").addClass("HdrNavRow-Container--down");
+			$(".MainHdr").addClass("nav--comin-in");
+		}
+		else {
+			$(".MainHdr").removeClass("nav--comin-in");
+			$(".MainHdr").removeClass("MainHdr--down");
+			$(".HdrNavRow-Container").removeClass("HdrNavRow-Container--down");
+			$(".MainHdr").addClass("nav--goin-out");
+		}
+	});
+	*/
+
+	win.scroll(function (event) {
+		if (win.scrollTop() < 100) {
+			$(".MainHdr").removeClass("nav--goin-out");
+			$(".MainHdr").removeClass("MainHdr--down");
+			$(".HdrNavRow").removeClass("HdrNavRow--down");
+			$(".MainHdr").addClass("nav--comin-in");
+		}
+		else {
+			$(".MainHdr").removeClass("nav--comin-in");
+			$(".MainHdr").addClass("MainHdr--down");
+			$(".HdrNavRow").addClass("HdrNavRow--down");
+			$(".MainHdr").addClass("nav--goin-out");
+		}
+	});
+
+
+	$(".MenuChoices-item").hover(
+		function () {
+			$(this).children(":first").fadeIn(500);
+		}, function () {
+			$(this).children(":first").fadeOut(100);
+		}
+		/***
+		***/
+	);
+});
